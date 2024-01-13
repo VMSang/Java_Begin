8ede43dd34f097ea012114842e0a75a5d5078246
VMSang <vosang20102005@gmail.com>
2024-01-13 12:28:51 +0700

    move

--- src/lab04chatgpt/SubEmployee.java
+++
@@ -1,9 +0,0 @@
-package lab04chatgpt;
-
-public class SubEmployee {
-	public static void main(String[] args) {
-		Employee nhanvien01 = new Employee();
-		Employee nv01 = nhanvien01.nhapThongTin();
-		nhanvien01.xuatThongtin(nv01);
-	}
-}
